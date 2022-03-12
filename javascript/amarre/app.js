342a0dcaa882220733446b320458dbbace6c1761
jose1824 <jguzman@innovaccion.mx>
2022-03-11 22:03:32 -0600

    Modo S activado

--- javascript/amarre/app.js
+++ javascript/amarre/app.js
@@ -1,22 +1,25 @@
-//sets a random absolute position to a html element; receives the html element 
-function moveElmRand(elm){ 
-    elm.style.position ='absolute'; 
-    elm.style.top = Math.floor(Math.random()*90+5)+'%'; 
-    elm.style.left = Math.floor(Math.random()*90+5)+'%'; 
-} 
-    
-//get the buttons
-var btn_no = document.querySelector('#btn_no');
+function moveElmRandom(elm) {
+    elm.style.position = 'absolute';
+    elm.style.top = Math.random() * (window.innerHeight - elm.offsetHeight) + 'px';
+    elm.style.left = Math.random() * (window.innerWidth - elm.offsetWidth) + 'px';
+}
+
 var btn_si = document.querySelector('#btn_si');
-    
-//register to call moveElmRand() on mouseenter event to #btn_no 
-btn_no.addEventListener('mouseenter', function(e){ moveElmRand(e.target);}); 
-    
-//register click to #btn_no 
-btn_no.addEventListener('click', function(e){ 
-    alert('¿Cómo de que no? 😡')
-    location.reload();
+var btn_no = document.querySelector('#btn_no');
+var modo_sexo_div = document.getElementsByClassName("modo_sexo")[0];
+
+btn_no.addEventListener('mouseenter', function(e) { moveElmRandom(e.target); });
+
+btn_si.addEventListener('click', function(e) { 
+    alert('Sabía que dirías que SI. Casemonos ya, tengamos hijos. TE AMO! 💖') 
+
+    modo_sexo_div.style.display = 'block';
+    const audio = new Audio('modo_hot.mp3');
+    audio.play();
 });
 
-btn_si.addEventListener('click', function(e){ alert("Sabia que si querías. Te amo, casemonos 💖")
+btn_no.addEventListener('click', function(e) {
+    const audio = new Audio('audio.mp3');
+    audio.play();
+    alert('¿Cómo de que no? 😡');
 });
